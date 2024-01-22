4a304e0fab1a17a1ad576da528314605e286e3c9
이미진 <stella@imijin-ui-MacBookPro.local>
2024-01-22 23:58:22 +0900

    [BUTTTO]UI 컴포넌트 작성 및 스토리북생성

--- src/stories/Atomic/Button.stories.ts
+++ src/stories/Atomic/Button.stories.ts
@@ -1,20 +1,75 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { Button } from "./Button";
+import Button from "@/components/atomic/Button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: "Atomic/Button",
   component: Button,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
+    componentSubtitle: "버튼 컴포넌트",
+    docs: {
+      description: {
+        component: "buttonType에 따라서 기본 스타일이 정의되어 있습니다.",
+      },
+    },
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    backgroundColor: { control: "color" },
+    type: {
+      description: "DOM버튼 기본 type에 대한 지정",
+      control: {
+        type: "select",
+        options: ["button", "submit", "reset"],
+      },
+    },
+    buttonType: {
+      description:
+        "버튼 용도에 맞추어 지정 - 사실 여기서는 기능에 정의된 스타일",
+      control: {
+        type: "select",
+        options: ["primary", "secondary", "notice"],
+      },
+    },
+    size: {
+      description: "버튼 사이즈에 대한 지정",
+      control: {
+        type: "select",
+        options: ["small", "medium", "large"],
+      },
+    },
+    disabled: {
+      description: "버튼 비활성화 여부에 대한 지정",
+      control: {
+        type: "boolean",
+      },
+    },
+    children: {
+      description: "버튼 내용에 대한 지정 - text or elelment",
+      control: {
+        type: "text",
+      },
+      table: {
+        type: {
+          summary: "string | ReactNode",
+        },
+      },
+    },
+    loading: {
+      description: "버튼 로딩 여부에 대한 지정",
+      control: {
+        type: "boolean",
+      },
+    },
+  },
+  args: {
+    //storybook v7 마이그레이션 이후부터  argTypes.defaultValues가 아닌 args로 변경됨
+    type: "button",
+    disabled: false,
+    loading: false,
   },
 } satisfies Meta<typeof Button>;
 
@@ -22,29 +77,49 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Default: Story = {
+  args: {
+    children: "정의된 공통스타일 외의 기본 버튼",
+  },
+};
+
 export const Primary: Story = {
   args: {
-    primary: true,
-    label: "Button",
+    buttonType: "primary",
+    size: "medium",
   },
 };
 
 export const Secondary: Story = {
   args: {
-    label: "Button",
+    buttonType: "secondary",
+    size: "medium",
+  },
+};
+export const Notice: Story = {
+  args: {
+    buttonType: "secondary",
+    size: "medium",
+  },
+};
+export const Disabled: Story = {
+  args: {
+    buttonType: "primary",
+    size: "medium",
+    disabled: true,
   },
 };
 
 export const Large: Story = {
   args: {
+    buttonType: "primary",
     size: "large",
-    label: "Button",
   },
 };
 
 export const Small: Story = {
   args: {
+    buttonType: "primary",
     size: "small",
-    label: "Button",
   },
 };
